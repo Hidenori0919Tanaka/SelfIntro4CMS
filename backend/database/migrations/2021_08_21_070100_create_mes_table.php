eaf3b0a064713c4e18fcb8bf4e4cc652a64b532d
TNK_Hidy <hidenori091985@yahoo.co.jp>
2021-08-21 16:11:53 +0900

    #11

---
+++ backend/database/migrations/2021_08_21_070100_create_mes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mes', function (Blueprint $table) {
+            $table->id();
+            $table->increments('id')->unsigned();
+			$table->integer('user_id')->nullable()->unsigned();
+			$table->text('title')->nullable();
+			$table->text('subtitle')->nullable();
+			$table->foreign("user_id")->references("id")->on("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mes');
+    }
+}
